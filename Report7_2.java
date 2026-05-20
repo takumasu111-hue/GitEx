c83d41caff6e8a9c8abd63fc78e49505a26fcbca
Ueno Takuma <takumasu111@icloud.com>
2026-05-21 02:35:18 +0900

    multiplyメソッドの説明コメントを追記

--- Report7_2.java
+++ Report7_2.java
@@ -3,9 +3,11 @@ package ex7_2;
 import java.util.Scanner;
 
 public class Report7_2 {
+	// ビット演算を使って2つの整数の乗算を行うメソッド
 	public static int multiply(int a, int b) {
 		int result = 0;
-		
+
+		 // b の最下位ビットが 1（奇数）の場合、現在の a を result に加算
 		while(b > 0) {
 			if((b & 1) != 0) {
 			   result = result + a;
